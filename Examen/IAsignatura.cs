5d1040134756dfa06d47ff4abd8144464b45d75b
50499 <50499@DESKTOP-4P1BP1K>
2024-11-06 18:41:28 -0600

    Agregar archivos de proyecto.

---
+++ Examen/IAsignatura.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    public interface IAsignatura
+    {
+        public double CalcularNotaFinal ();
+        public double CalcularPeriodo(int n1, int n2, int n3);
+        public string MensajeNotaFinal(double notaFinal);
+        public void Imprimir();
+    }
+}
